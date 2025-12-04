62bc51c6fe4a45789206c53c4afd997860fadf5e
pradeepsutharr <psuthar318@gmail.com>
2025-12-04 15:05:24 +0530

    Analytics

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -4,6 +4,7 @@ import SideBar from "@/layout/side-bar";
 import "@/styles/globals.css";
 import { Poppins } from "next/font/google";
 import { useRouter } from "next/router";
+import Script from "next/script";
 
 const font = Poppins({
   subsets: ["latin"],
@@ -12,49 +13,66 @@ const font = Poppins({
 });
 
 export default function App({ Component, pageProps }) {
+  const GA_ID = process.env.NEXT_PUBLIC_GA_ID;
   const router = useRouter();
-
   // check if this is an admin route
   const isAdminRoute = router.pathname.startsWith("/admin");
 
   return (
-    <div className={font.className}>
-      {isAdminRoute ? (
-        // -----------------------------
-        // ADMIN PAGES — NO LAYOUT
-        // -----------------------------
-        <div className="container bg-[#1e1e1f] p-10 rounded-2xl">
-          <div className="">
-            <Component {...pageProps} />
+    <>
+      <Script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+      ></Script>
+
+      <Script strategy="lazyOnload">
+        {`
+          // Google Analytics
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_ID}');
+        `}
+      </Script>
+
+      <div className={font.className}>
+        {isAdminRoute ? (
+          // -----------------------------
+          // ADMIN PAGES — NO LAYOUT
+          // -----------------------------
+          <div className="container bg-[#1e1e1f] p-10 rounded-2xl">
+            <div className="">
+              <Component {...pageProps} />
+            </div>
           </div>
-        </div>
-      ) : (
-        // -----------------------------
-        // NORMAL PUBLIC LAYOUT
-        // -----------------------------
-        <div className="layout ">
-          <div className="container ">
-            <div className="flex flex-wrap ">
-              <aside className="side-bar col-12 lg:col-3">
-                <SideBar />
-              </aside>
-              <div className="main-content col-12 lg:col-9 ">
-                <div className="bg-[#1e1e1f] border border-stroke rounded-2xl lg:rounded-3xl overflow-hidden lg:relative lg:px-8 lg:py-10 px-4 py-4 mb-20 md:mb-18 lg:mb-0 transition-all duration-transition-all duration-700">
-                  <div className="navbar hidden lg:flex absolute right-0 top-0">
-                    <Navbar />
-                  </div>
+        ) : (
+          // -----------------------------
+          // NORMAL PUBLIC LAYOUT
+          // -----------------------------
+          <div className="layout ">
+            <div className="container ">
+              <div className="flex flex-wrap ">
+                <aside className="side-bar col-12 lg:col-3">
+                  <SideBar />
+                </aside>
+                <div className="main-content col-12 lg:col-9 ">
+                  <div className="bg-[#1e1e1f] border border-stroke rounded-2xl lg:rounded-3xl overflow-hidden lg:relative lg:px-8 lg:py-10 px-4 py-4 mb-20 md:mb-18 lg:mb-0 transition-all duration-transition-all duration-700">
+                    <div className="navbar hidden lg:flex absolute right-0 top-0">
+                      <Navbar />
+                    </div>
 
-                  <div className="block lg:hidden">
-                    <MobileNavbar />
-                  </div>
+                    <div className="block lg:hidden">
+                      <MobileNavbar />
+                    </div>
 
-                  <Component {...pageProps} />
+                    <Component {...pageProps} />
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-      )}
-    </div>
+        )}
+      </div>
+    </>
   );
 }
